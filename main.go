912f37ea3c3cec044337c6ff85ee62dfebc3cbcb
Krishna <7krishna7dhakal7@gmail.com>
2021-06-22 17:05:29 +0545

    wasm calculator

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"strconv"
+	"syscall/js"
+)
+
+func add(this js.Value, i []js.Value) interface{} {
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1+int2)
+	return js.Global()
+
+}
+
+func subtract(this js.Value, i []js.Value) interface{} {
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1-int2)
+	return js.Global()
+
+}
+
+func registerCallbacks() {
+	js.Global().Set("add", js.FuncOf(add))
+	js.Global().Set("subtract", js.FuncOf(subtract))
+}
 
 func main() {
-	fmt.Println("Hello, WebAssembly!")
+	c := make(chan struct{}, 0)
+
+	println("WASM Go Initialized")
+	// register functions
+	registerCallbacks()
+	<-c
 }
